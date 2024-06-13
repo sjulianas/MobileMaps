f97ec4b151d9f18ee95bc1e0c5090331de31b1bf
Juliana Janu���rio <40135947812@>
2024-06-13 08:56:08 -0300

    13-06-24 v1

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native';
+import { View, Image, Text, ScrollView, Button } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 //1 - obter permição para o usuário acessar a localização
 //2- obter localização do usuário
@@ -15,11 +15,30 @@ import {
 import { styles } from './styles';
 import { useEffect, useState, useRef } from 'react'; //useRef - referencia para o mapa
 
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
   const [location, setLocation] = useState<LocationObject | null>(null);
 
   const mapRef = useRef<MapView>(null);
+
+  const latSensor = -22.914069;
+  const longSensor = -47.068136;
+
+  
+  const latSensor2 = -22.914199;
+  const longSensor2 = -47.068572;
+
+
+  const latSensor3 = -22.914208;
+  const longSensor3 = -47.068324;
+
+  const latSensor4 = -22.914280;
+  const longSensor4 = -47.068621;
+
+
 
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
@@ -51,7 +70,17 @@ export default function App() {
   }, []);
 
   return (
+    
     <View style={styles.container}>
+
+      <Image
+              source={require('./assets/Logo.png')}
+              style={{ width: '70%', marginBottom: 20 }} />
+
+      <Text style={{marginTop: 5, marginBottom:20, fontWeight: 600, fontSize: 20 }}>Localização dos Sensores</Text>
+      <Text style={{marginTop: -15, color: '#929395', marginBottom:30, fontWeight: 600, fontSize: 17 }}>Veja em tempo real os sensores!</Text>
+    
+
       {location && 
         <MapView 
           ref={mapRef}
@@ -63,6 +92,9 @@ export default function App() {
             longitudeDelta: 0.005
           }}
         >
+          
+          
+
           <Marker
             coordinate={{ //cordenada
               latitude: location.coords.latitude,
@@ -71,17 +103,52 @@ export default function App() {
           >
             <Image
               source={require('./assets/local.png')}
-              style={{ width: 40, height: 40 }} 
+              style={{ width: 20, height: 20 }} 
             />
           </Marker>
 
-            {/* <Image
-              source={require('./assets/sensor1.png')}
-              style={{ width: 40, height: 40 }} 
-            /> */}
+          <Marker pinColor='green' onPress={()=> {console.log("Cliquei");}}coordinate={{latitude: latSensor, longitude: longSensor}}>
+            <Image source={require('./assets/sensor1.png')} style={{ width: 30, height: 30 }} />
+          </Marker>
+
+          <Marker pinColor='red' onPress={()=> {console.log("Cliquei");}}coordinate={{latitude: latSensor2, longitude: longSensor2}}>
+            <Image source={require('./assets/sensor2.png')} style={{ width: 30, height: 30 }} />
+          </Marker>
+
+          <Marker pinColor='blue' onPress={()=> {console.log("Cliquei");}}coordinate={{latitude: latSensor3, longitude: longSensor3}}>
+            <Image source={require('./assets/sensor3.png')} style={{ width: 30, height: 30 }} />
+          </Marker>
+
+          <Marker pinColor='yellow' onPress={()=> {console.log("Cliquei");}}coordinate={{latitude: latSensor4, longitude: longSensor4}}>
+            <Image source={require('./assets/sensor4.png')} style={{ width: 30, height: 30 }} />
+          </Marker>
 
         </MapView>
+      
       }
+
+      <View>
+            <Text style={{marginTop: 30, fontSize: 17 }}>
+              Latitude: {location?.coords.latitude.toFixed(6)}
+            </Text>
+
+            <Text style={{marginTop: 5, fontSize: 17 }}>
+              Longitude: {location?.coords.longitude.toFixed(6)}
+            </Text>
+      </View>
+
+      
+      {/* <View>
+        <Text>
+          Sensor {id} com o tipo (tipo), de latitude (lat) e longitude (long)
+        </Text>
+      </View> */}
+
+    {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Ir para Sensores" onPress={() => navigation.navigate('Sensores')} // Navega para a tela de Sensores quando o botão é clicado
+      />
+    </View> */}
+
     </View>
   );
 }
